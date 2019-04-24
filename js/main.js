041da21b14ac2edc60c1847d52000d2378b15ed5
sylvainvant <sylvainvantieghem@gmail.com>
2019-04-24 22:50:49 +0200

    remove comment in main.js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,7 @@
 /**
  *  ToDo List with vue.js
  */
-/*
-new Vue({
-  el: '.todolist',
-  data: {
-    newtodolist:'',
-    todos:[
-    {newtodolist:'test'}
-    ]
-  }
-})
-*/
+
 new Vue ({
   el:"#app",
   data:{
