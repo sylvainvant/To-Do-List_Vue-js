ca96beebcc75b171099b08b8fa53cc5854a188fc
sylvainvant <sylvainvantieghem@gmail.com>
2019-04-24 22:45:29 +0200

    edit index.html li & add input + add in main.js data & methods

--- js/main.js
+++ js/main.js
@@ -1,10 +1,30 @@
 /**
- *  ToDo List
+ *  ToDo List with vue.js
  */
-
+/*
 new Vue({
-    el:'.ToDoList',
-    data: {
-        newlist:' '
-    }
+  el: '.todolist',
+  data: {
+    newtodolist:'',
+    todos:[
+    {newtodolist:'test'}
+    ]
+  }
 })
+*/
+new Vue ({
+  el:"#app",
+  data:{
+    todos:[
+      "toto",
+      "tata"
+    ]
+    },
+
+    methods:{
+      addTodo: function(){
+      this.todos.push(this.newList);
+      this.newList="";
+      }
+    }
+});
